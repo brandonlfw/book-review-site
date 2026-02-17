5eb4bf26017f980d246e82cf1e154edaa185fceb
Brandon <brandonwuedu@gmail.com>
2026-02-17 13:52:24 -0800

    Retrieve book reviews from Flask and display on browser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]); // books starts as empty array, setBooks updates that value
+
+  useEffect(() => {
+    fetch('/books')
+      .then(res => res.json()) // parses JSON response from Flask into JS array of books
+      .then(data => setBooks(data)); // setBooks(data) stores array of book objects into books state var
+  }, []);
+
   return (
     <div className="App">
       <h1>Book Reviews</h1>
+      {books.map(book => ( // loops thru each book and return the code under
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+          <p>Rating: {book.rating}/5</p>
+          <p>Reviewer: {book.reviewer}</p>
+        </div>
+      ))}
     </div>
   );
 }
